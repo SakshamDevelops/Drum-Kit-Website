6e87dec15e8dc40356794bf1f540bf770001ea56
SakshamDevelops <69129797+SakshamDevelops@users.noreply.github.com>
2020-12-06 11:33:54 +0530

    A major bug fixed🥳

--- index.js
+++ index.js
@@ -1,92 +1,237 @@
-var allButtons = document.querySelectorAll(".drum");
-for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  allButtons[i].addEventListener("click",
-    function() {
-      var buttonInnerHTML = this.innerHTML;
-      switch (buttonInnerHTML) {
+let gameState = "true";
 
-        case "w":
-          var tom1 = new Audio("sounds/tom-1.mp3");
-          tom1.play();
-          break;
+let player = "X";
 
-        case "a":
-          var tom3 = new Audio("sounds/tom-3.mp3");
-          tom3.play();
-          break;
+let cellMatrix = [
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+  ["", "", "", "", "", "", "", "", ""],
+];
+let bigWinCases =["", "", "", "", "", "", "", "", ""];
 
-        case "s":
-          var snare = new Audio("sounds/snare.mp3");
-          snare.play();
-          break;
 
-        case "d":
-          var kick = new Audio("sounds/kick-bass.mp3");
-          kick.play();
-          break;
-
-        case "j":
-          var crash = new Audio("sounds/crash.mp3");
-          crash.play();
-          break;
-
-        case "k":
-          var tom4 = new Audio("sounds/tom-4.mp3");
-          tom4.play();
-          break;
-
-        case "l":
-          var tom2 = new Audio("sounds/tom-2.mp3");
-          tom2.play();
-          break;
-
-        default:console.log(buttonInnerHTML)
-
-      }
-    }
-  );
+function win() {
+  return "Player " + player + " has won the game. Click on the Restart Button.";
 }
 
-document.addEventListener("keydown", function(event){
-   var innerAlphabet = event.key;
-   switch (innerAlphabet) {
-     case "w":
-       var tom1 = new Audio("sounds/tom-1.mp3");
-       tom1.play();
-       break;
+function draw() {
+  return "It's a Draw.";
+}
 
-     case "a":
-       var tom3 = new Audio("sounds/tom-3.mp3");
-       tom3.play();
-       break;
+function playerTurn() {
+  return "It's " + player + "'s turn.";
+}
 
-     case "s":
-       var snare = new Audio("sounds/snare.mp3");
-       snare.play();
-       break;
+const turn = document.querySelector(".game-status");
+var allBox = document.querySelectorAll(".game-box");
+document.querySelector(".restart-btn").addEventListener("click", restartFunc);
+document.querySelector(".game-box").style.backgroundColor = "black";
 
-     case "d":
-       var kick = new Audio("sounds/kick-bass.mp3");
-       kick.play();
-       break;
+for (let i = 0; i < 9; i++) {
+  for(let b=0; b<9;b++ ){
+  allBox[i].children[b].addEventListener("click", handleClick);
+  }
+}
 
-     case "j":
-       var crash = new Audio("sounds/crash.mp3");
-       crash.play();
-       break;
+function handleClick(event) {
+  const clickedBox = event.target.parentElement;
+  const clickedBoxNumber = parseInt(clickedBox.getAttribute("data-box-number"));
+  const clickedBoxColor = clickedBox.style.backgroundColor;
+  const clickedCell = event.target;
+  const clickedCellNumber = parseInt(clickedCell.getAttribute("data-cell-number"));
+  if (cellMatrix[clickedBoxNumber][clickedCellNumber] !== "" || clickedBoxColor == "" || clickedBoxColor =="white" || clickedBoxColor == "dimgrey") {
+    alert("You can't click on this box.");
+  }else{
+    handlePlay(clickedCell, clickedCellNumber,clickedBoxNumber,clickedBox);
+    resultCheck();
+  }
 
-     case "k":
-       var tom4 = new Audio("sounds/tom-4.mp3");
-       tom4.play();
-       break;
+}
 
-     case "l":
-       var tom2 = new Audio("sounds/tom-2.mp3");
-       tom2.play();
-       break;
+function handlePlay(clickedCell, clickedCellNumber, clickedBoxNumber, clickedBox) {
+  cellMatrix[clickedBoxNumber][clickedCellNumber] = player;
+  clickedCell.innerHTML = player;
+  document.querySelectorAll(".game-box")[clickedCellNumber].style.backgroundColor = "black";
+ 
+  if(clickedBoxNumber == clickedCellNumber){
+    clickedBox.style.backgroundColor = "black";
+  }
+  else{
+    for (let g = 0; g <9; g++) {
+      if( g == clickedCellNumber){
+        continue;
+      }
+    document.querySelectorAll(".game-box")[g].style.backgroundColor = "white";
+    for (let p = 0; p < 9; p++) {
+      document.querySelectorAll(".game-box")[g].children[p].style.borderColor = "black";
+      document.querySelectorAll(".game-box")[g].children[p].style.color = "black";
+    }
+    }
+  }
+  for (let l = 0; l <= 8; l++) {
+    document.querySelectorAll(".game-box")[clickedBoxNumber].children[l].style.borderColor = "black";
+    document.querySelectorAll(".game-box")[clickedBoxNumber].children[l].style.color = "black";
+    document.querySelectorAll(".game-box")[clickedCellNumber].children[l].style.borderColor = "white";
+    document.querySelectorAll(".game-box")[clickedCellNumber].children[l].style.color = "white";
+  }
+  
+}
 
-     default:console.log(innerAlphabet);
 
+const winningCases = [
+
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+
+];
+
+function resultCheck() {
+  smallWin();
+  bigWin();
+  const clickedCell = event.target;
+  const clickedCellNumber = parseInt(clickedCell.getAttribute("data-cell-number"));
+  var targetColor = document.querySelectorAll(".game-box")[clickedCellNumber].style.backgroundColor;
+  if (targetColor == "dimgrey"){
+   colorAllExceptGrey();
+  }
+}
+
+function handleNextPlayer() {
+
+  if (player == "X") {
+    player = "O";
+    turn.innerHTML = playerTurn();
+  } else {
+    player = "X";
+    turn.innerHTML = playerTurn();
+  }
+
+}
+
+function restartFunc() {
+  gameState = true;
+  cellMatrix = [
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", ""],
+  ];
+  bigWinCases =["", "", "", "", "", "", "", "", ""];
+  player = "X";
+  turn.innerHTML = playerTurn();
+  for (let i = 0; i < document.querySelectorAll(".game-cell").length; i++) {
+    var allCells = document.querySelectorAll(".game-cell");
+    allCells[i].innerHTML = "";
+  }
+  for (let w = 1; w <9 ; w++) {
+   document.querySelectorAll(".game-box")[w].style.backgroundColor = "white";
+
+  for (let a = 0; a < 9; a++) {
+    document.querySelectorAll(".game-box")[w].children[a].style.borderColor = "black";
+    document.querySelectorAll(".game-box")[w].children[a].style.color = "black";
+    document.querySelector(".game-box").children[a].style.borderColor = "white";
+    document.querySelector(".game-box").children[a].style.color = "white";
    }
- }
-)
+  }
+
+   document.querySelector(".game-box").style.backgroundColor = "black";
+   for (let i = 0; i < 9; i++) {
+    for(let b=0; b<9;b++ ){
+    allBox[i].children[b].addEventListener("click", handleClick);
+    }
+  }
+
+}
+ 
+function smallWin(){
+  for (let i = 0; i <= 7; i++) {
+    const winCondition = winningCases[i];
+    for (let q = 0; q < 9; q++) {
+      let a = cellMatrix[q][winCondition[0]];
+      let b = cellMatrix[q][winCondition[1]];
+      let c = cellMatrix[q][winCondition[2]];
+      if (a === "" || b === "" || c === "") {
+        continue;
+      }
+      if (a === b && b === c) {
+        document.querySelectorAll(".game-box")[q].style.backgroundColor = "dimgrey";  
+        bigWinCases[q] = a ;
+      }    
+    }
+  }
+}
+
+
+function bigWin(){
+ let roundWon= false;
+  for (let a = 0; a <=7; a++) {
+    const bigWinCondition = winningCases[a];
+    for (let m = 0; m < 9; m++) {
+      let one = bigWinCases[bigWinCondition[0]];
+      let two = bigWinCases[bigWinCondition[1]];
+      let three = bigWinCases[bigWinCondition[2]];
+      if (one === "" || two === "" || three === ""){
+        continue;
+      }
+      if(one == two && two == three){
+        roundWon=true;
+        break;  
+      }
+    }
+  }
+  if(roundWon){
+    turn.innerHTML =win();
+    alert("Player " + player + " has won the game.");
+    for (let i = 0; i < 9; i++) {
+      for(let b=0; b<9;b++ ){
+      allBox[i].children[b].removeEventListener("click", handleClick);
+      }
+    }
+    return;
+  }
+    let roundDraw = bigWinCases.includes("");
+    if (!roundDraw) {
+      turn.innerHTML = draw();
+      gameState = false;
+      return;
+    }
+
+  handleNextPlayer();
+
+}
+function colorAllExceptGrey() {
+  for (let z = 0; z < 9; z++) {
+     let checkGrey = document.querySelectorAll(".game-box")[z].style.backgroundColor ;
+    if(checkGrey == "dimgrey"){
+      for (let t = 0; t < 9; t++) {
+        document.querySelectorAll(".game-box")[z].children[t].style.borderColor = "black";
+        document.querySelectorAll(".game-box")[z].children[t].style.color = "black";
+       }
+    }
+    else{
+      document.querySelectorAll(".game-box")[z].style.backgroundColor = "black";
+      for (let y = 0; y < 9; y++) {
+      document.querySelectorAll(".game-box")[z].children[y].style.borderColor = "white";
+      document.querySelectorAll(".game-box")[z].children[y].style.color = "white"; 
+      }
+    }
+    
+  }
+}
